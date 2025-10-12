e0927d48a0094fafe85ad361498920635cfb788f
Vibhanshu-555 <166938116+Vibhanshu-555@users.noreply.github.com>
2025-10-12 20:50:39 +0530

    Update 03_clean_brand.sql

--- sql/data_cleaning/03_clean_brand.sql
+++ sql/data_cleaning/03_clean_brand.sql
@@ -1,14 +1,15 @@
--- Create a new table events_clean_step1 with price > 0
+-- Brick 7.2: Brand Cleaning
+-- Create a new table events_clean_step2 with cleaned brand
 
-CREATE OR REPLACE TABLE `infra-throne-471611-v9.ecommerce_analytics.events_clean_step1` AS
-SELECT *
-FROM `infra-throne-471611-v9.ecommerce_analytics.events_staging`
-WHERE price > 0;
-
---Sanity Checks
+CREATE OR REPLACE TABLE `infra-throne-471611-v9.ecommerce_analytics.events_clean_step2` AS
 SELECT 
-  COUNT(*) AS total_rows,
-  COUNTIF(price = 0) AS zero_price,
-  MIN(price) AS min_price,
-  MAX(price) AS max_price
+    event_time,
+    event_type,
+    product_id,
+    category_id,
+    category_code,
+    IFNULL(LOWER(brand), "unknown") AS brand,  -- replace NULL with 'unknown' & lowercase
+    price,
+    user_id,
+    user_session
 FROM `infra-throne-471611-v9.ecommerce_analytics.events_clean_step1`;
