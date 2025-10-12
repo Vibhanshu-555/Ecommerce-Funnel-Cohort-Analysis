9db51d3fd176f8570c0515a3eedba213148c7e0f
Vibhanshu-555 <166938116+Vibhanshu-555@users.noreply.github.com>
2025-10-12 21:27:51 +0530

    Update 05_final_cleaned_table.sql

--- sql/data_cleaning/05_final_cleaned_table.sql
+++ sql/data_cleaning/05_final_cleaned_table.sql
@@ -1,21 +1,10 @@
--- Create a new table events_clean_step2 with cleaned brand
+-- Brick 7.4: Split category_code hierarchy
+-- Create a table with main, sub1, sub2 categories
 
-CREATE OR REPLACE TABLE `infra-throne-471611-v9.ecommerce_analytics.events_clean_step2` AS
+CREATE OR OR REPLACE TABLE `infra-throne-471611-v9.ecommerce_analytics.events_clean_final` AS
 SELECT 
-    event_time,
-    event_type,
-    product_id,
-    category_id,
-    category_code,
-    IFNULL(LOWER(brand), "unknown") AS brand,  -- replace NULL with 'unknown' & lowercase
-    price,
-    user_id,
-    user_session
-FROM `infra-throne-471611-v9.ecommerce_analytics.events_clean_step1`;
-
-
-SELECT 
-  COUNT(*) AS total_rows,
-  COUNTIF(brand IS NULL) AS null_brand,
-  COUNT(DISTINCT brand) AS unique_brands
-FROM `infra-throne-471611-v9.ecommerce_analytics.events_clean_step2`;
+    *,
+    SPLIT(category_code, ".")[OFFSET(0)] AS main_category,
+    SPLIT(category_code, ".")[OFFSET(1)] AS sub_category1,
+    SPLIT(category_code, ".")[OFFSET(2)] AS sub_category2
+FROM `infra-throne-471611-v9.ecommerce_analytics.events_clean_step3`;
